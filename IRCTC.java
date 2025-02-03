d6abc523445ebe326f7e8fd6bc39bed97d6fc36a
darshil dobariya <darshild2121@gmail.com>
2025-02-03 16:47:36 +0530

    project started

--- IRCTC.java
+++ IRCTC.java
@@ -1,6 +1,4 @@
-class train{
 
-}
 
 public class IRCTC {
     public static void main(String[] args) {
---
+++ Train.java
@@ -0,0 +1,8 @@
+public class Train {
+    static int numOfTrain=0;
+    int trainId;
+    String trainName;
+    int numOfCoaches;
+    int capacityIn1Coach;
+
+}
